2b0f60b1b01ee6706df91c0800c793d879dfe2ee
Carl Simon Adorf <csadorf@umich.edu>
2020-10-06 11:19:49 +0200

    Refactor page generation.
     - Split app-data fetching and app page generation.
     - Remove obsolete code segments and imports.

--- make_ghpages/make_pages.py
+++ make_ghpages/make_pages.py
@@ -5,7 +5,7 @@ import json
 import os
 import shutil
 import string
-from collections import OrderedDict, defaultdict
+from collections import OrderedDict
 from pathlib import Path
 from urllib.parse import urlparse
 from urllib.request import urlopen
@@ -22,7 +22,7 @@ templates_folder = 'templates'
 static_folder = 'static'
 
 # outputs
-out_folder = 'out' 
+out_folder = 'out'
 html_subfolder_name = 'apps' # subfolder for HTMLs of apps
 apps_meta_file = 'apps_meta.json'
 
@@ -82,16 +82,16 @@ def get_git_branches(git_url):
 
 def get_git_author(git_url):
     git_author = urlparse(git_url).path
-    
+
     git_author = git_author.split('/')[1]
 
     ## Special condition, only valid when git_author is 'aiidalab'
     if git_author == 'aiidalab':
         git_author = 'AiiDA Lab Team'
-    
+
     return git_author
 
-def validate_meta_info(app_name, meta_info, git_url):
+def complete_meta_info(app_name, meta_info, git_url):
     if 'state' not in meta_info:
         meta_info['state'] = 'registered'
 
@@ -128,6 +128,54 @@ def get_logo_url(logo_rel_path, meta_url):
     return logo_url
 
 
+def fetch_app_data(app_data, app_name):
+    # Get Git URL, fail build if git_url is not found or wrong
+    if 'git_url' in app_data:
+        hosted_on = get_hosted_on(app_data['git_url'])
+    else:
+        raise exc.MissingGit("No 'git_url' key for '{}' in apps.json".format(app_name))
+
+    # Get metadata.json from the project;
+    # fail build if meta_url is not found or wrong
+    if 'meta_url' in app_data:
+        meta_info = get_meta_info(app_data['meta_url'])
+    else:
+        raise exc.MissingMetadata("No 'meta_url' key for '{}' in apps.json".format(app_name))
+
+    # Check if categories are specified, warn if not
+    if 'categories' not in app_data:
+        print("  >> WARNING: No categories specified.")
+    # Check correct categories are specified
+    else:
+        validate_categories(app_data['categories'], apps_meta['categories'])
+
+    app_data['metainfo'] = complete_meta_info(app_name, meta_info, app_data['git_url'])
+    app_data['gitinfo'] = get_git_branches(app_data['git_url'])
+    app_data['hosted_on'] = hosted_on
+
+    # Get logo URL, if it has been specified
+    if 'logo' in app_data['metainfo']:
+        app_data['logo'] = get_logo_url(app_data['metainfo']['logo'], app_data['meta_url'])
+
+    return app_data
+
+
+def generate_apps_meta(apps_raw_data, categories_raw_data):
+    apps_meta = {}
+    apps_meta['apps'] = OrderedDict()
+    apps_meta['categories'] = categories_raw_data
+    print("Fetch app data...")
+    for app_name in sorted(apps_raw_data.keys()):
+        print("  - {}".format(app_name))
+        app_data = fetch_app_data(apps_raw_data[app_name], app_name)
+        app_data['name'] = app_name
+        app_data['subpage'] = os.path.join(html_subfolder_name,
+                              get_html_app_fname(app_name))
+        apps_meta['apps'][app_name] = app_data
+
+    return apps_meta
+
+
 if __name__ == "__main__":
     pwd = os.path.split(os.path.abspath(__file__))[0]
     outdir_abs = os.path.join(pwd, out_folder)
@@ -155,68 +203,25 @@ if __name__ == "__main__":
     with open(os.path.join(pwd, os.pardir, categories_file)) as f:
         categories_raw_data = json.load(f)
 
-    all_data = {}
-    all_data['apps'] = OrderedDict()
-    all_data['categories'] = categories_raw_data
+    apps_meta = generate_apps_meta(apps_raw_data, categories_raw_data)
 
     html_subfolder_abs = os.path.join(outdir_abs, html_subfolder_name)
     os.mkdir(html_subfolder_abs)
 
-    summaries = defaultdict(list)
-    other_summary = []
-    other_summary_names = set()
-
+    # Make single-entry page based on singlepage.html
     print("[apps]")
-    for app_name in sorted(apps_raw_data.keys()):
-        print("  - {}".format(app_name))
-        app_data = apps_raw_data[app_name]
-        app_data['name'] = app_name
-
-        html_app_fname = get_html_app_fname(app_name)
-        subpage_name = os.path.join(html_subfolder_name,
-                                    get_html_app_fname(app_name))
+    for app_name, app_data in apps_meta['apps'].items():
+        subpage_name = app_data['subpage']
         subpage_abspath = os.path.join(outdir_abs, subpage_name)
 
-        # Get Git URL, fail build if git_url is not found or wrong
-        if 'git_url' in app_data:
-            hosted_on = get_hosted_on(app_data['git_url'])
-        else:
-            raise exc.MissingGit("No 'git_url' key for '{}' in apps.json".format(app_name))
-
-        # Get metadata.json from the project;
-        # fail build if meta_url is not found or wrong
-        if 'meta_url' in app_data:
-            meta_info = get_meta_info(app_data['meta_url'])
-        else:
-            raise exc.MissingMetadata("No 'meta_url' key for '{}' in apps.json".format(app_name))
-
-        # Check if categories are specified, warn if not
-        if 'categories' not in app_data:
-            print("  >> WARNING: No categories specified.")
-        # Check correct categories are specified
-        else:
-            validate_categories(app_data['categories'], all_data['categories'])
-
-        app_data['metainfo'] = validate_meta_info(app_name, meta_info, app_data['git_url'])
-        app_data['gitinfo'] = get_git_branches(app_data['git_url'])
-        app_data['subpage'] = subpage_name
-        app_data['hosted_on'] = hosted_on
-        
-        # Get logo URL, if it has been specified
-        if 'logo' in app_data['metainfo']:
-            app_data['logo'] = get_logo_url(app_data['metainfo']['logo'], app_data['meta_url'])
-
-        all_data['apps'][app_name] = app_data
-
-        # Make single-entry page based on singlepage.html
-        app_html = singlepage_template.render(category_map=all_data['categories'], **app_data)
+        app_html = singlepage_template.render(category_map=apps_meta['categories'], **app_data)
         with codecs.open(subpage_abspath, 'w', 'utf-8') as f:
             f.write(app_html)
-        print("    - Page {} generated.".format(subpage_name))
+        print("  - {} generated.".format(subpage_name))
 
     # Make index page based on main_index.html
     print("[main index]")
-    rendered = main_index_template.render(**all_data)
+    rendered = main_index_template.render(**apps_meta)
     outfile = os.path.join(outdir_abs, 'index.html')
     with codecs.open(outfile, 'w', 'utf-8') as f:
         f.write(rendered)
@@ -225,7 +230,7 @@ if __name__ == "__main__":
     # Save json data for the app manager
     outfile = os.path.join(outdir_abs, apps_meta_file)
     with codecs.open(outfile, 'w', 'utf-8') as f:
-        json.dump(all_data, f, ensure_ascii=False, indent=2)
+        json.dump(apps_meta, f, ensure_ascii=False, indent=2)
     print("  - apps_meta.json generated")
 
     # Copy schemas
