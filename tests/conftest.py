57db86c716767fd830a56f5c492c569afb849188
Simon Adorf <carl.simon.adorf@gmail.com>
2021-03-18 19:00:19 +0100

    consolidate tests

---
+++ tests/conftest.py
@@ -0,0 +1,55 @@
+import json
+from functools import partial
+from pathlib import Path
+
+import jsonschema
+import pytest
+
+ROOT = Path(__file__).parent.parent.resolve()
+
+
+@pytest.fixture
+def validate():
+    return partial(jsonschema.validate, format_checker=jsonschema.draft7_format_checker)
+
+
+@pytest.fixture
+def apps_schema():
+    return json.loads(ROOT.joinpath("schemas/apps.schema.json").read_text())
+
+
+@pytest.fixture
+def apps_meta_schema():
+    return json.loads(ROOT.joinpath("schemas/apps_meta.schema.json").read_text())
+
+
+@pytest.fixture
+def categories_schema():
+    return json.loads(ROOT.joinpath("schemas/categories.schema.json").read_text())
+
+
+@pytest.fixture
+def metadata_schema():
+    return json.loads(ROOT.joinpath("schemas/metadata.schema.json").read_text())
+
+
+@pytest.fixture
+def mock_schema_endpoints(
+    requests_mock, apps_schema, apps_meta_schema, categories_schema, metadata_schema
+):
+    requests_mock.get(
+        "https://aiidalab.github.io/aiidalab-registry/schemas/v2/apps.schema.json",
+        text=json.dumps(apps_schema),
+    )
+    requests_mock.get(
+        "https://aiidalab.github.io/aiidalab-registry/schemas/v2/metadata.schema.json",
+        text=json.dumps(metadata_schema),
+    )
+    requests_mock.get(
+        "https://aiidalab.github.io/aiidalab-registry/schemas/v2/apps_meta.schema.json",
+        text=json.dumps(apps_meta_schema),
+    )
+    requests_mock.get(
+        "https://aiidalab.github.io/aiidalab-registry/schemas/v2/categories.schema.json",
+        text=json.dumps(categories_schema),
+    )
---
+++ tests/test_build.py
@@ -0,0 +1,129 @@
+# -*- coding: utf-8 -*-
+
+import pytest
+import base64
+from dataclasses import dataclass
+
+import requests
+
+import app_registry
+
+
+@pytest.fixture
+def app_git_url():
+    return "https://github.com/aiidalab/aiidalab-hello-world.git"
+
+
+@pytest.fixture
+def app_metadata_url():
+    """A URL for the test app metadata."""
+    return "https://raw.githubusercontent.com/aiidalab/aiidalab-hello-world/master/metadata.json"
+
+
+@pytest.fixture
+def app_logo_url():
+    """A URL for the test app logo."""
+    return "https://raw.githubusercontent.com/aiidalab/aiidalab-hello-world/master/img/logo.png"
+
+
+@pytest.fixture
+def app_logo_img():
+    """A one-pixel large 'logo' for the test app."""
+    return base64.b64decode(
+        b"iVBORw0KGgoAAAANSUhEUgAAAAEAAAABAQMAAAAl21bKAAAAA1BMVEX/TQBcNTh/AAAAAXRSTlPM0jRW/QAAAApJREFUeJxjYgAAAAYAAzY3fKgAAAAASUVORK5CYII="
+    )
+
+
+@pytest.fixture
+def app_logo(requests_mock, app_logo_url, app_logo_img):
+    @dataclass
+    class AppLogo:
+        url = app_logo_url
+        img = app_logo_img
+
+    requests_mock.get(app_logo_url, content=app_logo_img)
+    return AppLogo
+
+
+@pytest.fixture
+def app_metadata(requests_mock, app_logo):
+    """Create metadata content for a test app."""
+    return {
+        "description": "A test app that does not really exist.",
+        "title": "Test App",
+        "version": "1.0.0",
+        "authors": "AiiDAlab team",
+        "logo": app_logo.url,
+        "state": "development",
+    }
+
+
+@pytest.fixture
+def apps_yaml(requests_mock, app_git_url, app_metadata):
+    """Create apps.yaml content with one test app entry."""
+    apps_yaml = {
+        "test": {
+            "git_url": app_git_url,
+            "metadata": app_metadata,
+            "categories": ["utilities"],
+        }
+    }
+    requests_mock.get(app_git_url)
+    yield apps_yaml
+
+
+@pytest.fixture
+def categories_yaml():
+    """Create categories.yaml content."""
+    return {
+        "utilities": {
+            "title": "Utilities",
+            "description": "Utility apps for everyday tasks.",
+        }
+    }
+
+
+@pytest.fixture
+def app_registry_data(apps_yaml, categories_yaml):
+    """Create app registry data content."""
+    return app_registry.AppRegistryData(apps=apps_yaml, categories=categories_yaml)
+
+
+@pytest.fixture
+def app_registry_schemas(apps_schema, categories_schema):
+    """Create app registry schema content."""
+    return app_registry.AppRegistrySchemas(
+        apps=apps_schema, categories=categories_schema
+    )
+
+
+@pytest.mark.usefixtures("mock_schema_endpoints")
+def test_generate_apps_meta(app_registry_data, apps_meta_schema):
+    apps_meta = app_registry.generate_apps_meta(
+        data=app_registry_data, schema=apps_meta_schema
+    )
+    # Very basic validation here, the apps_meta.json file is already validated via the schema:
+    assert "apps" in apps_meta
+    assert "categories" in apps_meta
+
+    # Check that the test app metadata is present.
+    assert "test" in apps_meta["apps"]
+    assert (
+        apps_meta["apps"]["test"]["git_url"]
+        == app_registry_data.apps["test"]["git_url"]
+    )
+    assert all(
+        cat in apps_meta["categories"]
+        for cat in apps_meta["apps"]["test"]["categories"]
+    )
+
+
+@pytest.mark.usefixtures("mock_schema_endpoints")
+def test_get_logo_url(app_registry_data, app_logo_url, apps_meta_schema):
+    """Test whether the logo url is correctly resolved."""
+    apps_meta = app_registry.generate_apps_meta(
+        data=app_registry_data, schema=apps_meta_schema
+    )
+    assert apps_meta["apps"]["test"]["logo"] == app_logo_url
+    r = requests.get(app_logo_url)
+    assert r.status_code == 200
