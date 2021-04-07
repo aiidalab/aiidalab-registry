e944d5f824cb13cd95ee1e38bf8bf55f554afe77
Simon Adorf <carl.simon.adorf@gmail.com>
2021-04-07 18:18:29 +0200

    Revert "Provide additional data for downstream users."
    This reverts commit 35cbc07552e77b92ad84ce67a80864c12e121cb0.

--- src/app_registry/metadata.py
+++ src/app_registry/metadata.py
@@ -4,7 +4,6 @@
 import logging
 import os
 from collections import OrderedDict
-from urllib.parse import urldefrag
 
 import jsonschema
 
@@ -39,13 +38,8 @@ def fetch_app_data(app_data, app_name):
     app_data["metadata"] = complete_metadata(app_name, app_data["metadata"], git_url)
     if git_url:
         app_data["gitinfo"] = util.get_git_branches(git_url)
-        app_data["path"] = f"${{AIIDALAB_APPS}}/{app_name}/"
         app_data["releases"] = {
-            release: {
-                "dependencies": dependencies,
-                "install_command": f"rm -rf ${{AIIDALAB_APPS}}/{app_name}/ && git clone --depth 1 --branch={release} {urldefrag(git_url).url} ${{AIIDALAB_APPS}}/{app_name}/",
-                "uninstall_command": f"rm -rf ${{AIIDALAB_APPS}}/{app_name}/",
-            }
+            release: {"dependencies": dependencies}
             for release, dependencies in find_releases_and_dependencies(app_data)
         }
     if hosted_on:
